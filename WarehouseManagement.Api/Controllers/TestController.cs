6a579977661e9cbbf25ddb567179dbcf7c5ad650
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-05-20 12:47:02 +0300

    test-endpoint

---
+++ WarehouseManagement.Api/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WarehouseManagement.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+    [HttpGet("testjson")]
+    public IActionResult GetTestJson()
+    {
+        var testData = new
+        {
+            Id = 1,
+            Name = "Test Item",
+            Description = "This is a test item",
+            Attributes = new List<string> { "Attribute1", "Attribute2", "Attribute3" },
+            CreatedAt = DateTime.UtcNow
+        };
+
+        return Ok(testData);
+    }
+}
