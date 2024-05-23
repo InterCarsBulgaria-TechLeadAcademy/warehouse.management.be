3a23d3519f0cd73f8963631728333b681ffeb9f2
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-05-23 21:19:32 +0300

    Documented repository methods

--- WarehouseManagement.Infrastructure/Data/Common/IRepository.cs
+++ WarehouseManagement.Infrastructure/Data/Common/IRepository.cs
@@ -61,18 +61,54 @@ namespace WarehouseManagement.Infrastructure.Data.Common
         /// <param name="entities">The list of entities to delete.</param>
         void DeleteRange<T>(IEnumerable<T> entities)
             where T : class;
+
+        /// <summary>
+        /// Asynchronously retrieves an entity of type T, including soft-deleted entities, from the database based on the specified id.
+        /// </summary>
+        /// <param name="id">The id of the entity to retrieve.</param>
+        /// <returns>Returns the retrieved entity or null if not found.</returns>
         Task<T?> GetByIdWithDeletedAsync<T>(int id)
             where T : BaseClass;
+
+        /// <summary>
+        /// Retrieves all records of type T, including soft-deleted entities, from the database without tracking changes.
+        /// </summary>
+        /// <returns>An IQueryable representing the queryable expression tree without change tracking.</returns>
         IQueryable<T> AllWithDeletedReadOnly<T>()
             where T : BaseClass;
+
+        /// <summary>
+        /// Retrieves all records of type T, including soft-deleted entities, from the database.
+        /// </summary>
+        /// <returns>An IQueryable representing the queryable expression tree.</returns>
         IQueryable<T> AllWithDeleted<T>()
             where T : BaseClass;
-        void SoftDelete<T>(T entity, string userId)
+
+        /// <summary>
+        /// Soft deletes the specified entity from the database.
+        /// </summary>
+        /// <param name="entity">The entity to soft delete.</param>
+        void SoftDelete<T>(T entity)
             where T : BaseClass;
-        Task SoftDeleteById<T>(object id, string userId)
+
+        /// <summary>
+        /// Asynchronously soft deletes the entity with the specified id from the database.
+        /// </summary>
+        /// <param name="id">The id of the entity to soft delete.</param>
+        Task SoftDeleteById<T>(object id)
             where T : BaseClass;
+
+        /// <summary>
+        /// Restores a soft deleted entity in the database.
+        /// </summary>
+        /// <param name="entity">The entity to restore.</param>
+
         void UnDelete<T>(T entity)
             where T : BaseClass;
+
+        /// <summary>
+        /// Asynchronously saves all changes made in the database context, along with logging the changes.
+        /// </summary>
         Task<int> SaveChangesWithLogAsync(CancellationToken cancellationToken = default);
     }
 }
