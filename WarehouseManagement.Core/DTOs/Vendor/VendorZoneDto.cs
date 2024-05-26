411ed533d43b4744dec3a77e84de80bb3dfadedb
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2024-05-26 10:10:49 +0300

    VendorDto VendorMarkerDto and VendorZoneDto

---
+++ WarehouseManagement.Core/DTOs/Vendor/VendorZoneDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WarehouseManagement.Core.DTOs.Vendor
+{
+    public class VendorZoneDto
+    {
+        public int ZoneId { get; set; }
+        public string ZoneName { get; set; } = string.Empty;
+        public bool IsFinal { get; set; }
+    }
+}
