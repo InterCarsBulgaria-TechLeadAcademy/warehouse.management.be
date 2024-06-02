02848a4dc6e343ebe92040f4514401c23134c538
Spasko Katsarski <patsokatsarski1@gmail.com>
2024-06-02 11:11:52 +0300

    Add functionality for Zones
    Adding ZoneController, ZoneServices and DTOs

---
+++ WarehouseManagement.Api/Controllers/ZoneController.cs
@@ -0,0 +1,136 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using WarehouseManagement.Core.Contracts;
+using WarehouseManagement.Core.DTOs.Zone;
+
+namespace WarehouseManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ZoneController : ControllerBase
+    {
+        private readonly IZoneService zoneService;
+
+        public ZoneController(IZoneService zoneService)
+        {
+            this.zoneService = zoneService;
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(200, Type = typeof(ZoneDto))]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                var model = await this.zoneService.GetByIdAsync(id);
+
+                return Ok(model);
+            }
+            catch (KeyNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        [HttpGet("all")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<ZoneDto>))]
+        public async Task<IActionResult> GetAll()
+        {
+            var zones = await this.zoneService.GetAllAsync();
+
+            return Ok(zones);
+        }
+
+        [HttpPost("add")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> Add([FromBody] ZoneFormDto model)
+        {
+            try
+            {
+                await this.zoneService.CreateAsync(model, User.Id());
+
+                return Ok("Zone with name {model.Name} was successfully added");
+            }
+            catch (ArgumentException ae)
+            {
+                return BadRequest(ae.Message);
+            }
+        }
+
+        [HttpPut("edit/{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> Edit(int id, [FromBody] ZoneFormDto model)
+        {
+            try
+            {
+                await zoneService.EditAsync(id, model, User.Id());
+
+                return Ok("Zone successfully edited");
+            }
+            catch (KeyNotFoundException ke)
+            {
+                return NotFound(ke.Message);
+            }
+            catch (ArgumentException ae)
+            {
+                return BadRequest(ae.Message);
+            }
+        }
+
+        [HttpDelete("delete/{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await zoneService.DeleteAsync(id, User.Id());
+            }
+            catch (KeyNotFoundException ke)
+            {
+                return NotFound(ke.Message);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return Ok("Zone was deleted successfully");
+        }
+
+        [HttpPut("restore/{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> Restore(int id)
+        {
+            try
+            {
+                var name = await zoneService.RestoreAsync(id);
+
+                return Ok($"Zone {name} was restored");
+            }
+            catch (KeyNotFoundException ke)
+            {
+                return NotFound(ke.Message);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("deleted")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<ZoneDto>))]
+        public async Task<IActionResult> AllDeleted()
+        {
+            var model = await zoneService.GetAllWithDeletedAsync();
+            return Ok(model);
+        }
+    }
+}
---
+++ WarehouseManagement.Core/Services/ZoneService.cs
@@ -0,0 +1,234 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WarehouseManagement.Core.Contracts;
+using WarehouseManagement.Core.DTOs.Zone;
+using WarehouseManagement.Infrastructure.Data.Common;
+using WarehouseManagement.Infrastructure.Data.Models;
+
+namespace WarehouseManagement.Core.Services;
+
+public class ZoneService : IZoneService
+{
+    private readonly IRepository repository;
+
+    public ZoneService(IRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    public async Task CreateAsync(ZoneFormDto model, string userId)
+    {
+        if (await this.ExistsByNameAsync(model.Name))
+        {
+            throw new ArgumentException($"Zone with name '{model.Name}' already exists.");
+        }
+
+        var zone = new Zone()
+        {
+            Name = model.Name,
+            CreatedAt = DateTime.UtcNow,
+            CreatedByUserId = userId
+        };
+
+        await this.repository.AddAsync(zone);
+        await this.repository.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id, string userId)
+    {
+        if (!await this.ExistsByIdAsync(id))
+        {
+            throw new KeyNotFoundException($"Zone with ID '{id}' not found.");
+        }
+
+        var zone = (await this.repository.GetByIdAsync<Zone>(id))!;
+
+        if (zone.ZonesMarkers.Any())
+        {
+            var markers = zone.ZonesMarkers.Select(zm => zm.Marker.Name);
+
+            throw new InvalidOperationException(
+                $"Zone cannot be deleted because it has existing markers: {string.Join(",", markers)}"
+            );
+        }
+
+        if (zone.VendorsZones.Any())
+        {
+            var vendors = zone.VendorsZones.Select(vz => vz.Vendor.Name);
+
+            throw new InvalidOperationException(
+                $"Zone cannot be deleted because it has existing vendors: {string.Join(",", vendors)}."
+            );
+        }
+
+        if (zone.Entries.Any())
+        {
+            throw new InvalidOperationException(
+                "Zone cannot be deleted because it has existing entries."
+            );
+        }
+
+        zone.DeletedByUserId = userId;
+        zone.DeletedAt = DateTime.UtcNow;
+
+        this.repository.SoftDelete(zone);
+        await this.repository.SaveChangesAsync();
+    }
+
+    public async Task EditAsync(int id, ZoneFormDto model, string userId)
+    {
+        if (!await this.ExistsByIdAsync(id))
+        {
+            throw new KeyNotFoundException($"Zone with ID '{id}' not found.");
+        }
+
+        if (!await this.ExistsByNameAsync(model.Name))
+        {
+            throw new ArgumentException($"Zone with the name '{model.Name}' already exists.");
+        }
+
+        var zone = (await this.repository.GetByIdAsync<Zone>(id))!;
+
+        zone.Name = model.Name;
+        zone.LastModifiedAt = DateTime.UtcNow;
+        zone.LastModifiedByUserId = userId;
+
+        await this.repository.SaveChangesWithLogAsync();
+    }
+
+    public async Task<bool> ExistsByIdAsync(int id)
+    {
+        return await this.repository.GetByIdAsync<Zone>(id) != null;
+    }
+
+    public async Task<bool> ExistsByNameAsync(string name)
+    {
+        return await this.repository.AllReadOnly<Zone>().AnyAsync(z => z.Name == name);
+    }
+
+    public async Task<IEnumerable<ZoneDto>> GetAllAsync()
+    {
+        return await this
+            .repository.AllReadOnly<Zone>()
+            .Select(z => new ZoneDto()
+            {
+                Id = z.Id,
+                Name = z.Name,
+                Markers = z.ZonesMarkers.Select(zm => new ZoneMarkerDto()
+                {
+                    MarkerId = zm.MarkerId,
+                    MarkerName = zm.Marker.Name,
+                }),
+                Vendors = z.VendorsZones.Select(vz => new ZoneVendorDto()
+                {
+                    VendorId = vz.VendorId,
+                    VendorName = vz.Vendor.Name,
+                    VendorSystemNumber = vz.Vendor.SystemNumber
+                }),
+                Entries = z.Entries.Select(e => new ZoneEntryDto()
+                {
+                    Pallets = e.Pallets,
+                    Packages = e.Packages,
+                    Pieces = e.Pieces,
+                    StartedProccessing = e.StartedProccessing,
+                    FinishedProccessing = e.FinishedProccessing,
+                    DeliveryId = e.DeliveryId
+                })
+            })
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<ZoneDto>> GetAllWithDeletedAsync()
+    {
+        return await this
+            .repository.AllWithDeletedReadOnly<Zone>()
+            .Select(z => new ZoneDto()
+            {
+                Id = z.Id,
+                Name = z.Name,
+                Markers = z.ZonesMarkers.Select(zm => new ZoneMarkerDto()
+                {
+                    MarkerId = zm.MarkerId,
+                    MarkerName = zm.Marker.Name,
+                }),
+                Vendors = z.VendorsZones.Select(vz => new ZoneVendorDto()
+                {
+                    VendorId = vz.VendorId,
+                    VendorName = vz.Vendor.Name,
+                    VendorSystemNumber = vz.Vendor.SystemNumber
+                }),
+                Entries = z.Entries.Select(e => new ZoneEntryDto()
+                {
+                    Pallets = e.Pallets,
+                    Packages = e.Packages,
+                    Pieces = e.Pieces,
+                    StartedProccessing = e.StartedProccessing,
+                    FinishedProccessing = e.FinishedProccessing,
+                    DeliveryId = e.DeliveryId
+                })
+            })
+            .ToListAsync();
+    }
+
+    public async Task<ZoneDto> GetByIdAsync(int id)
+    {
+        if (!await this.ExistsByIdAsync(id))
+        {
+            throw new KeyNotFoundException($"Zone with ID '{id}' not found.");
+        }
+
+        var zone = (
+            await this.repository.AllReadOnly<Zone>().FirstOrDefaultAsync(z => z.Id == id)
+        )!;
+
+        return new ZoneDto()
+        {
+            Id = zone.Id,
+            Name = zone.Name,
+            Markers = zone.ZonesMarkers.Select(zm => new ZoneMarkerDto()
+            {
+                MarkerId = zm.MarkerId,
+                MarkerName = zm.Marker.Name,
+            }),
+            Vendors = zone.VendorsZones.Select(vz => new ZoneVendorDto()
+            {
+                VendorId = vz.VendorId,
+                VendorName = vz.Vendor.Name,
+                VendorSystemNumber = vz.Vendor.SystemNumber
+            }),
+            Entries = zone.Entries.Select(e => new ZoneEntryDto()
+            {
+                Pallets = e.Pallets,
+                Packages = e.Packages,
+                Pieces = e.Pieces,
+                StartedProccessing = e.StartedProccessing,
+                FinishedProccessing = e.FinishedProccessing,
+                DeliveryId = e.DeliveryId
+            })
+        };
+    }
+
+    public async Task<string> RestoreAsync(int id)
+    {
+        var zone = await this.repository.All<Zone>().FirstOrDefaultAsync(v => v.Id == id);
+
+        if (zone == null)
+        {
+            throw new KeyNotFoundException($"Vendor with ID '{id}' not found.");
+        }
+
+        if (!zone.IsDeleted)
+        {
+            throw new InvalidOperationException($"Zone '{zone.Name}' is not deleted");
+        }
+
+        if (await this.ExistsByNameAsync(zone.Name))
+        {
+            throw new InvalidOperationException($"Zone with name '{zone.Name}' already exists.");
+        }
+
+        this.repository.UnDelete(zone);
+        await repository.SaveChangesAsync();
+
+        return zone.Name;
+    }
+}
