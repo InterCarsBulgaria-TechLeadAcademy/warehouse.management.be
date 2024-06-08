be072a71be87d9753c14944dbbd7a01d2c91d5e8
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2024-06-08 16:34:54 +0300

    Some Tests Added

--- Warehouse.Management.Tests/VendorServiceTests.cs
+++ Warehouse.Management.Tests/VendorServiceTests.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Moq;
 using WarehouseManagement.Api.Services.Contracts;
+using WarehouseManagement.Common.MessageConstants.Keys;
 using WarehouseManagement.Core.Contracts;
+using WarehouseManagement.Core.DTOs;
+using WarehouseManagement.Core.DTOs.Vendor;
 using WarehouseManagement.Core.Services;
 using WarehouseManagement.Infrastructure.Data;
 using WarehouseManagement.Infrastructure.Data.Common;
@@ -38,6 +41,7 @@ public class VendorServiceTests
         {
             Id = 1,
             Name = "Vendor1",
+            SystemNumber = "Sm1",
             CreatedAt = DateTime.UtcNow.AddDays(-1),
             CreatedByUserId = "user1"
         };
@@ -45,6 +49,7 @@ public class VendorServiceTests
         {
             Id = 2,
             Name = "Vendor2",
+            SystemNumber = "Sm2",
             CreatedAt = DateTime.UtcNow.AddDays(-2),
             CreatedByUserId = "user2"
         };
@@ -52,6 +57,7 @@ public class VendorServiceTests
         {
             Id = 3,
             Name = "Vendor3",
+            SystemNumber = "Sm3",
             CreatedAt = DateTime.UtcNow.AddDays(-3),
             CreatedByUserId = "user3"
         };
@@ -59,6 +65,7 @@ public class VendorServiceTests
         {
             Id = 4,
             Name = "vendorWithMarkers",
+            SystemNumber = "Sm4",
             CreatedAt = DateTime.UtcNow.AddDays(-3),
             CreatedByUserId = "user3"
         };
@@ -66,6 +73,7 @@ public class VendorServiceTests
         {
             Id = 5,
             Name = "vendorWithZones",
+            SystemNumber = "Sm5",
             CreatedAt = DateTime.UtcNow.AddDays(-3),
             CreatedByUserId = "user3"
         };
@@ -73,6 +81,7 @@ public class VendorServiceTests
         {
             Id = 6,
             Name = "vendorWithZonesAndMarkes",
+            SystemNumber = "Sm6",
             CreatedAt = DateTime.UtcNow.AddDays(-3),
             CreatedByUserId = "user3"
         };
@@ -80,15 +89,23 @@ public class VendorServiceTests
         {
             Id = 7,
             Name = "deletedVendor1",
+            SystemNumber = "Sm7",
             CreatedAt = DateTime.UtcNow.AddDays(-3),
-            CreatedByUserId = "user3"
+            CreatedByUserId = "user3",
+            IsDeleted = true,
+            DeletedAt = DateTime.UtcNow,
+            DeletedByUserId = "adminUserId"
         };
         deletedVendor2 = new Vendor()
         {
             Id = 8,
             Name = "deletedVendor2",
+            SystemNumber = "Sm8",
             CreatedAt = DateTime.UtcNow.AddDays(-3),
-            CreatedByUserId = "user3"
+            CreatedByUserId = "user3",
+            IsDeleted = true,
+            DeletedAt = DateTime.UtcNow,
+            DeletedByUserId = "adminUserId"
         };
 
         marker1 = new Marker()
@@ -199,12 +216,184 @@ public class VendorServiceTests
     }
 
     [Test]
-    public void Test()
+    public async Task GetByIdAsync_ShouldReturnDtoModelIfIdExist()
     {
-        var a = 1;
-        var b = -1;
+        var existingId = vendor1.Id;
+        var result = await vendorService.GetByIdAsync(existingId);
 
-        Assert.That(a, Is.EqualTo(1));
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.Id, Is.EqualTo(existingId));
+        Assert.That(result.Name, Is.EqualTo(vendor1.Name));
+    }
+
+    [Test]
+    public void GetByIdAsync_ShouldThrowKeyNotFoundExceptionIfVendorDoesNotExist()
+    {
+        var nonExistingId = -1;
+
+        var ex = Assert.ThrowsAsync<KeyNotFoundException>(
+            async () => await vendorService.GetByIdAsync(nonExistingId)
+        );
+
+        Assert.That(
+            ex.Message,
+            Is.EqualTo($"{VendorMessageKeys.VendorWithIdNotFound} {nonExistingId}")
+        );
+    }
+
+    [Test]
+    public async Task GetAllAsync_ReturnsDtoCollection()
+    {
+        var paginationParamas = new PaginationParameters()
+        {
+            PageNumber = 1,
+            PageSize = int.MaxValue,
+            SearchQuery = null
+        };
+
+        var expectedCount = 6;
+        var result = await vendorService.GetAllAsync(paginationParamas);
+
+        Assert.That(result.Count, Is.EqualTo(expectedCount));
+    }
+
+    [Test]
+    public async Task GetAllAsync_ReturnsFilteredDtoCollection_WhenPaginationIsApllied()
+    {
+        var paginationParamas1 = new PaginationParameters()
+        {
+            PageNumber = 1,
+            PageSize = 2,
+            SearchQuery = null
+        };
+        var paginationParamas2 = new PaginationParameters()
+        {
+            PageNumber = 1,
+            PageSize = 3,
+            SearchQuery = null
+        };
+
+        var result1 = await vendorService.GetAllAsync(paginationParamas1);
+        var result2 = await vendorService.GetAllAsync(paginationParamas2);
+
+        Assert.That(result1.Count, Is.LessThanOrEqualTo(paginationParamas1.PageSize));
+        Assert.That(result2.Count, Is.LessThanOrEqualTo(paginationParamas2.PageSize));
+    }
+
+    [Test]
+    public async Task GetAllAsync_ReturnsFilteredDtoCollection_WhenSearchByVendorNameIsApplied()
+    {
+        var searchedVendorSystemNumber = "Sm1";
+        var searchTermCaseInsensitivy = "sM1";
+        var searchTermCaseSensitivy = "Sm1";
+
+        var result1 = await vendorService.GetAllAsync(
+            new PaginationParameters()
+            {
+                PageNumber = 1,
+                PageSize = 2,
+                SearchQuery = searchTermCaseInsensitivy
+            }
+        );
+
+        var result2 = await vendorService.GetAllAsync(
+            new PaginationParameters()
+            {
+                PageNumber = 1,
+                PageSize = 2,
+                SearchQuery = searchTermCaseSensitivy
+            }
+        );
+
+        Assert.IsTrue(result1.All(m => m.SystemNumber.Contains(searchedVendorSystemNumber)));
+        Assert.IsTrue(result2.All(m => m.SystemNumber.Contains(searchedVendorSystemNumber)));
+    }
+
+    [Test]
+    public async Task GetAllAsync_ReturnsFilteredDtoCollection_WhenSearchByVendorSystemNumberIsApplied()
+    {
+        var searchedVendorName = "Vendor1";
+        var searchTermCaseInsensitivy = "veNdOr1";
+        var searchTermCaseSensitivy = "Vendor1";
+
+        var result1 = await vendorService.GetAllAsync(
+            new PaginationParameters()
+            {
+                PageNumber = 1,
+                PageSize = 2,
+                SearchQuery = searchTermCaseInsensitivy
+            }
+        );
+
+        var result2 = await vendorService.GetAllAsync(
+            new PaginationParameters()
+            {
+                PageNumber = 1,
+                PageSize = 2,
+                SearchQuery = searchTermCaseSensitivy
+            }
+        );
+
+        Assert.IsTrue(result1.All(m => m.Name.Contains(searchedVendorName)));
+        Assert.IsTrue(result2.All(m => m.Name.Contains(searchedVendorName)));
+    }
+
+    [Test]
+    public async Task AddAsync_ShouldAddNewVendorToDb()
+    {
+        var newVendor = new Vendor()
+        {
+            Name = "newVendor",
+            SystemNumber = "newVendorSystemNumber",
+            CreatedAt = DateTime.UtcNow,
+            CreatedByUserId = "someUserId",
+        };
+
+        var vendorCountBeforeAdding = await dbContext.Vendors.CountAsync();
+        await dbContext.AddAsync(newVendor);
+        await dbContext.SaveChangesAsync();
+        var result = await dbContext.Vendors.CountAsync();
+
+        Assert.That(result, Is.GreaterThan(vendorCountBeforeAdding));
+        Assert.That(result, Is.EqualTo(vendorCountBeforeAdding + 1));
+    }
+
+    [Test]
+    public void AddAsync_ShouldThrowArgumentException_IfVendorWithNameAlreadyExist()
+    {
+        var newVendor = new VendorFormDto()
+        {
+            Name = vendor1.Name,
+            SystemNumber = "newVendorSystemNumber"
+        };
+
+        var ex = Assert.ThrowsAsync<ArgumentException>(
+            async () => await vendorService.AddAsync(newVendor, "userId")
+        );
+
+        Assert.That(
+            ex.Message,
+            Is.EqualTo($"{VendorMessageKeys.VendorWithNameExist} {newVendor.Name}")
+        );
+    }
+
+    [Test]
+    public void AddAsync_ShouldThrowArgumentException_IfVendorWithSystemNumberAlreadyExist()
+    {
+        var newVendor = new VendorFormDto()
+        {
+            Name = "someTestName",
+            SystemNumber = vendor1.SystemNumber
+        };
+
+        var ex = Assert.ThrowsAsync<ArgumentException>(
+            async () => await vendorService.AddAsync(newVendor, "userId")
+        );
+
+        Assert.That(
+            ex.Message,
+            Is.EqualTo($"{VendorMessageKeys.VendorWithSystemNumberExist} {newVendor.SystemNumber}")
+        );
     }
 
     [TearDown]
