dc48216e5f9ff0692d87fb1369c6664e12a381cc
Spasko Katsarski <patsokatsarski1@gmail.com>
2024-07-12 16:32:08 +0300

    Add MaxLength validation attribute to DifferenceType entity

--- WarehouseManagement.Infrastructure/Data/Models/DifferenceType.cs
+++ WarehouseManagement.Infrastructure/Data/Models/DifferenceType.cs
@@ -1,6 +1,10 @@
-﻿namespace WarehouseManagement.Infrastructure.Data.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using static WarehouseManagement.Common.ValidationConstants.DifferenceTypeConstants;
+
+namespace WarehouseManagement.Infrastructure.Data.Models;
 
 public class DifferenceType : BaseClass
 {
+    [MaxLength(NameMaxLenght)]
     public string Name { get; set; } = string.Empty;
 }
