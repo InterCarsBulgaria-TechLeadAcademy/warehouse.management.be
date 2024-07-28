6b22213f43f22b5607794734731d91ecb402f982
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2024-07-28 14:28:59 +0300

    unused usings removed

--- WarehouseManagement.Core/Services/PDFService.cs
+++ WarehouseManagement.Core/Services/PDFService.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Drawing.Imaging;
-using System.IO;
 using iText.IO.Font.Constants;
 using iText.IO.Image;
 using iText.Kernel.Colors;
