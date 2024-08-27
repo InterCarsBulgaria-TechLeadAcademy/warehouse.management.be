d202785278d381b75a7c9dfe6861aa1a28e66644
vexleet <abda70226@gmail.com>
2024-08-27 19:40:05 +0300

    remove factories try

--- WarehouseManagement.Core/Contracts/IDeliveryHistoryChangeFactory.cs
+++
@@ -1,9 +0,0 @@
-using WarehouseManagement.Core.DTOs.Delivery;
-using WarehouseManagement.Infrastructure.Data.Models;
-
-namespace WarehouseManagement.Core.Contracts;
-
-public interface IDeliveryHistoryChangeFactory
-{
-    public DeliveryHistoryDto CreateDeliveryHistoryChange(EntityChange change);
-}
